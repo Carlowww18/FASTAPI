e214f7fd04e36478b2ed0db04b3d740f70286a9c
Carlowww18 <carlowww38@gmail.com>
2025-08-18 13:25:06 -0300

    gerenciamento de usuario

--- routers/auth_routes.py
+++ routers/auth_routes.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from models.models import Usuario, db
+from dependencies import pegar_sessao
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -7,4 +9,14 @@ async def auth():
     return {"mensagem": "Você acessou a rota de auth",
             "autenticado": False}
 
-@auth_router
+@auth_router.post("/criar_conta")
+async def criar_conta(email: str, senha: str, nome: str, session = Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+
+    if usuario:
+          return {"mensagem": "Já existe um usuário com esse"}
+    else:
+         novo_usuario = Usuario(nome, email, senha)
+         session.add(novo_usuario)
+         session.commit()
+         return {"mensagem": "Usuário cadastrado com sucesso"}
