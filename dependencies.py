f461fe93aff1cdb49ba06c3e2d2c20e9bb6e5276
Carlowww18 <carlowww38@gmail.com>
2025-08-20 14:49:32 -0300

    Inserindo criptografia de senhas com bcrypt

--- dependencies.py
+++ dependencies.py
@@ -1,6 +1,5 @@
-from models.models import Usuario, db
+from models.models import db
 from sqlalchemy.orm import sessionmaker
-
 
 def pegar_sessao():
     try:    
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 app = FastAPI()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 from routers.auth_routes import auth_router
 from routers.order_routes import order_router
